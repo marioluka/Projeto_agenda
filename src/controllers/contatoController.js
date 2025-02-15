dd97a58a4437386e9828f5b0620bfab16068d0bb
marioluka <marioluka88@gmail.com>
2025-02-15 18:28:50 -0300

    Update, read e delete dos contatos

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -2,7 +2,7 @@ const Contato = require('../models/ContatoModel');
 
 exports.index = (req, res) => {
     res.render('contato', {
-        contato: {} 
+        contato: {}
     });
 };
 
@@ -22,9 +22,9 @@ exports.register = async (req, res) => {
         req.flash('success', 'Contato registrado com sucesso.');
         req.session.save(() => {
             res.location(req.get("Referrer") || "/");
-            return res.redirect(`./index/${contato.contato._id}`);
+            return res.redirect(`/contato/index/${contato.contato._id}`);
         });
-        
+
     } catch (e) {
         console.log(e);
         return res.render('404');
@@ -37,3 +37,43 @@ exports.editIndex = async (req, res) => {
     if (!contato) return res.render('404');
     res.render('contato', { contato });
 };
+
+exports.edit = async (req, res) => {
+    try {
+        if (!req.params.id) return res.render('404');
+        const contato = new Contato(req.body);
+        await contato.edit(req.params.id);
+
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => {
+                res.location(req.get("Referrer") || "/");
+                return res.redirect('./contato/edit');
+            });
+            return;
+        }
+
+        req.flash('success', 'Contato editado com sucesso.');
+        req.session.save(() => {
+            res.location(req.get("Referrer") || "/");
+            return res.redirect(`/contato/index/${contato.contato._id}`);
+        });
+        return;
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.delete = async (req, res) => {
+    if (!req.params.id) return res.render('404');
+    const contato = await Contato.delete(req.params.id);
+    if (!contato) return res.render('404');
+
+    req.flash('success', 'Contato apagado com sucesso.');
+    req.session.save(() => {
+        res.location(req.get("Referrer") || "/");
+        return res.redirect(`/`);
+    });
+    return;
+}
